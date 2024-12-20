fd07f4f50bf55a2bb45b9a4745c87a25f1865ce2
Roncelli <leonardoroncelli@gmail.com>
2024-12-20 19:20:53 +0100

    Metodi get e set e costruttore della classe Furgone.

--- src/Furgone.java
+++ src/Furgone.java
@@ -3,4 +3,35 @@ public class Furgone extends Veicolo{
     private int numeroAssi;
     private double altezzaMax;
     private String alimentazione;
+    public Furgone(String targa,String marca,String modello,double peso,int annoImmatricolazione,double capacitaCarico,int numeroAssi,double altezzaMax,String alimentazione){
+        super(targa, marca, modello, peso, annoImmatricolazione);
+        this.capacitaCarico=capacitaCarico;
+        this.numeroAssi=numeroAssi;
+        this.altezzaMax=altezzaMax;
+        this.alimentazione=alimentazione;
+    }
+    public double getCapacitaCarico() {
+        return capacitaCarico;
+    }
+    public void setCapacitaCarico(double capacitaCarico) {
+        this.capacitaCarico = capacitaCarico;
+    }
+    public int getNumeroAssi() {
+        return numeroAssi;
+    }
+    public void setNumeroAssi(int numeroAssi) {
+        this.numeroAssi = numeroAssi;
+    }
+    public double getAltezzaMax() {
+        return altezzaMax;
+    }
+    public void setAltezzaMax(double altezzaMax) {
+        this.altezzaMax = altezzaMax;
+    }
+    public String getAlimentazione() {
+        return alimentazione;
+    }
+    public void setAlimentazione(String alimentazione) {
+        this.alimentazione = alimentazione;
+    }
 }
