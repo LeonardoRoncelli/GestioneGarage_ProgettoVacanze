c23ef3d6c64033d081b64dd237906fd3771946df
Roncelli <leonardoroncelli@gmail.com>
2024-12-20 19:04:56 +0100

    Metodi get e set e costruttore della classe Box.

--- src/Box.java
+++ src/Box.java
@@ -2,4 +2,27 @@ public class Box {
     private int numeroBox;
     private String tipologiaBox;
     private boolean isOccupato;
+    public void setNumeroBox(int numeroBox){
+        this.numeroBox=numeroBox;
+    }
+    public int getNumeroBox(){
+        return numeroBox;
+    }
+    public void setTipologiaBox(String tipologiaBox){
+        this.tipologiaBox=tipologiaBox;
+    }
+    public String getTipologiaBox(){
+        return tipologiaBox;
+    }
+    public void setIsOccupato(boolean isOccupato){
+        this.isOccupato=isOccupato;
+    }
+    public boolean getIsOccupato(){
+        return isOccupato;
+    }
+    public Box(int numeroBox,String tipologiaBox,boolean isOccupato){
+        this.numeroBox=numeroBox;
+        this.tipologiaBox=tipologiaBox;
+        this.isOccupato=isOccupato;
+    }
 }
