a04133ae1713a97efbd743e1df0d7a70daa08f07
Roncelli <leonardoroncelli@gmail.com>
2024-12-20 19:18:06 +0100

    Metodi get e set e costruttore della classe Moto.

--- src/Moto.java
+++ src/Moto.java
@@ -3,4 +3,35 @@ public class Moto extends Veicolo{
     private String tipologia;
     private int numTempi;
     private double capienzaSerbatoio;
+    public Moto(String targa,String marca,String modello,double peso,int annoImmatricolazione,int cilindrata,String tipologia,int numTempi,double capienzaSerbatoio){
+        super(targa, marca, modello, peso, annoImmatricolazione);
+        this.cilindrata=cilindrata;
+        this.tipologia=tipologia;
+        this.numTempi=numTempi;
+        this.capienzaSerbatoio=capienzaSerbatoio;
+    }
+    public int getCilindrata() {
+        return cilindrata;
+    }
+    public void setCilindrata(int cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+    public String getTipologia() {
+        return tipologia;
+    }
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
+    public int getNumTempi() {
+        return numTempi;
+    }
+    public void setNumTempi(int numTempi) {
+        this.numTempi = numTempi;
+    }
+    public double getCapienzaSerbatoio() {
+        return capienzaSerbatoio;
+    }
+    public void setCapienzaSerbatoio(double capienzaSerbatoio) {
+        this.capienzaSerbatoio = capienzaSerbatoio;
+    }
 }
